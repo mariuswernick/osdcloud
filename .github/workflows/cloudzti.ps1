b3c6378032ef8633638e8c4bd8d4d7f28eb7e832
mariuswernick <90559349+mariuswernick@users.noreply.github.com>
2022-05-30 16:33:08 +0200

    Update cloudzti.ps1

--- .github/workflows/cloudzti.ps1
+++ .github/workflows/cloudzti.ps1
@@ -27,7 +27,7 @@ Connect-AzOSDCloud
 Connect-AzAccount -UseDeviceAuthentication -AuthScope Storage -ErrorAction Stop
 
 # Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 22H2 -OSEdition Enterprise -ZTI
-Start-OSDCloud -Product NODRIVER -ZTI -ImageFileUrl "https://osdcloudbechtlev.blob.core.windows.net/server/de-de-server2022.wim" -ImageIndex 2
+Start-OSDCloud -Product NODRIVER -ZTI -ImageFileUrl "https://osdcloudbechtlev.blob.core.windows.net/server/de-de-server2022.wim?sp=r&st=2022-05-30T14:32:08Z&se=2022-05-30T22:32:08Z&spr=https&sv=2020-08-04&sr=b&sig=AqcBGheH%2FscLPKxzDPO40BxEHJzYw%2FMq9liD%2F4DDXtU%3D" -ImageIndex 2
 
 # Set Drive Lable Name
 Set-Volume -DriveLetter C -NewFileSystemLabel "Windows"
