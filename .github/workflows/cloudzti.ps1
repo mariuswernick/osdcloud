d825b3f6f564b902f2ba1e3c7099b271bb11f19d
mariuswernick <90559349+mariuswernick@users.noreply.github.com>
2022-05-31 09:13:24 +0200

    Update cloudzti.ps1

--- .github/workflows/cloudzti.ps1
+++ .github/workflows/cloudzti.ps1
@@ -19,12 +19,6 @@ Import-Module OSD -Force
 #import AzCloud functions
 iex (irm functions.osdcloud.com)
 
-#Connect AzOSDCloud
-Connect-AzOSDCloud
-
-
-#Connect To AzureAD
-Connect-AzAccount -UseDeviceAuthentication -AuthScope Storage -ErrorAction Stop
 
 # Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 22H2 -OSEdition Enterprise -ZTI
 Start-OSDCloud -Product NODRIVER -ZTI -ImageFileUrl "https://osdcloudbechtlev.blob.core.windows.net/server/de-de-server2022.wim?sp=r&st=2022-05-30T14:32:08Z&se=2022-05-30T22:32:08Z&spr=https&sv=2020-08-04&sr=b&sig=AqcBGheH%2FscLPKxzDPO40BxEHJzYw%2FMq9liD%2F4DDXtU%3D" -ImageIndex 2
